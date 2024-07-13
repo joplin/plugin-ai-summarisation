597040afb8f11bdb3630d332299294d627f9785b
Bill Nguyen Ton Hoang <bill.nguyentonhoang@live.com>
2024-07-13 16:51:09 +0100

    upgrade: adding styles to the panel -> ready for the full development of the panel

--- src/ui/panel.ts
+++ src/ui/panel.ts
@@ -1,5 +1,7 @@
 import joplin from 'api';
 
+const fs = require('fs-extra');
+
 export class SummarisationPanel {
     public panelInstance;
 
@@ -22,6 +24,15 @@ export class SummarisationPanel {
         this.panelInstance = await joplin.views.panels.create('summary-ai-panel');
         const panelHtml: string = await this.generatePanelHtml();
         await joplin.views.panels.setHtml(this.panelInstance, panelHtml);
+
+        const installationDir = await joplin.plugins.installationDir();
+        const files = await fs.promises.readdir(installationDir + '/ui/panel_react/styles/');
+        const cssFiles = files.filter((file) => file.endsWith('.css')).map((file) => 'ui/panel_react/styles/' + file);
+
+        for(const css of cssFiles) {
+            await joplin.views.panels.addScript(this.panelInstance, css);
+        }
+
         await joplin.views.panels.addScript(this.panelInstance, './ui/panel_react/index.js');
         await joplin.views.panels.show(this.panelInstance);
     }
--- src/ui/panel_react/app.tsx
+++ src/ui/panel_react/app.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export function App() {
+export default function App() {
 
     return (
         <div className='summarization-ai-panel'>
